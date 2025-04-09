19fb04998bc0121b7f39f4873cad72b2c67b6fb0
hmel1990 <hmel408757595@gmail.com>
2025-04-09 18:58:01 +0300

    Добавьте файлы проекта.

---
+++ TextAnalyzeDZ/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace TextAnalyzeDZ
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("1 - Запустить анализ\n2 - Остановить\n3 - Выход\n\n");
+                var key = Console.ReadLine();
+
+                if (key == "1")
+                {
+                    cts = new CancellationTokenSource();
+                    StartAnalysis(cts.Token);
+                }
+                else if (key == "2")
+                {
+                    cts.Cancel();
+                    Console.WriteLine("Анализ остановлен.");
+                }
+                else if (key == "3")
+                {
+                    break;
+                }
+            }
+
+        }
+        private static CancellationTokenSource? cts;
+        static void StartAnalysis(CancellationToken token)
+        {
+            Task.Run(() => AnalyzeText(token));
+        }
+
+        static void AnalyzeText(CancellationToken token)
+        {
+            try
+            {
+                var sr = new StreamReaderFile("E:\\STEP\\SystemProg\\DZ\\TextAnalyzeDZ\\TextAnalyzeDZ\\bin\\Debug\\net9.0\\1.txt");
+                var text = sr.readText();
+
+                Task.Run(() => sr.countSentences(text, token));
+                Task.Run(() => sr.countSymbols(text, token));
+                Task.Run(() => sr.countWords(text, token));
+                Task.Run(() => sr.countQuestions(text, token));
+                Task.Run(() => sr.countExclamation(text, token));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+    }
+}
+
+//3.Приложение для анализа текста
+//Создайте приложение, которое использует механизм параллельных тасков. Приложение должно открывать большой текстовый файл,
+//проанализировать текст, и вывести отчёт, который содержит информацию о:
+//-Количестве предложений
+//- Количестве символов
+//- Количестве слов
+//- Количестве вопросительных предложений
+//- Количестве восклицательных предложений
+//Отчёт должен быть отображён на экране, по мере готовности каждого таска.
+//Также добавьте возможность остановки и повторного запуска анализа текста.
---
+++ TextAnalyzeDZ/StreamReaderFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace TextAnalyzeDZ
+{
+    internal class StreamReaderFile
+    {
+        public string ?path { get; set; }
+        public string readText()
+        {
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var sr = new StreamReader(fs);
+            var text = sr.ReadToEnd();
+            fs.Close();
+            
+            return text;
+        }
+
+        public int countSentences(string text, CancellationToken token)
+        {
+            var sent = text.Count (c=> c=='.');
+            Console.WriteLine($"Количество предложений: {sent}\n");
+            return sent;
+        }
+        public int countSymbols(string text, CancellationToken token)
+        {
+            var symbols = text.Length;
+            Console.WriteLine($"Количество символов: {symbols}\n");
+            return symbols;
+        }
+        public int countWords(string text, CancellationToken token)
+        {
+            var words = text.Count(c => c == ' ');
+            Console.WriteLine($"Количество слов: {words}\n");
+            return words;
+        }
+        public int countQuestions(string text, CancellationToken token)
+        {
+            var quest = text.Count(c => c == '?');
+            Console.WriteLine($"Количество вопросительных предложений: {quest}\n");
+            return quest;
+        }
+
+        public int countExclamation(string text, CancellationToken token)
+        {
+            var excl = text.Count(c => c == '!');
+            Console.WriteLine($"Количество восклицательных предложений: {excl}\n");
+            return excl;
+        }
+
+        public StreamReaderFile (string path)
+        {
+            this.path = path;
+        }
+            
+    }
+}
